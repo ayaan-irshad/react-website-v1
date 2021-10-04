8f6047bb6e9f820ddab2c907e2f1ce1910f22f68
ayaan-irshad <ayaanirshadbhat@gmail.com>
2021-10-04 20:27:18 +0530

    added footer data

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import './Footer.css';
-import { Button } from './Button';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./Footer.css";
+import { Button } from "./Button";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <div className='footer-container'>
-      
-     
-     
-      
+    <div className="footer-container">
+      <div>
+        <h1>Contact Us</h1>
+        <span>Email</span> <i class="far fa-envelope"></i>:
+        fmsfreightbrokers@gmail.com
+        <span>Phone</span>
+        <i class="fas fa-phone"></i> : 916923945
+        <span>Adress</span> <i class="fas fa-map-marker-alt"></i>: PO bocx 3309,
+        Sacrememto , California 1233
+      </div>
     </div>
   );
 }
