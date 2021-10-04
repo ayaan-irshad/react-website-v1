e8fd42741af118c9d7471fc2202b1398206801a8
ayaan-irshad <ayaanirshadbhat@gmail.com>
2021-10-04 19:50:06 +0530

    logo update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,6 +8,7 @@ function Footer() {
     <div className='footer-container'>
       
      
+     
       
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,7 +30,7 @@ function Navbar() {
         <div className='navbar-container'>
           <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
             FMS
-            <i class='fa-solid fa-truck-container' />
+            <i class='fas fa-truck' />
           </Link>
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
