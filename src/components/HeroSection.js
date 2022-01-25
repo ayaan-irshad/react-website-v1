c1bdff0cdacd9a2b481eb1fe1f734b7889676010
Ayaan Irshad <54441228+ayaan-irshad@users.noreply.github.com>
2022-01-25 23:38:21 +0530

    changes about contact us details and get quote button

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import '../App.css';
-import { Button } from './Button';
-import './HeroSection.css';
+import React from "react";
+import "../App.css";
+import { Button } from "./Button";
+import "./HeroSection.css";
 
 function HeroSection() {
   return (
-    <div className='hero-container'>
-      <img src='/img/img-1.jpg'  />
+    <div className="hero-container">
+      <img src="/img/img-1.jpg" />
       <h1>Best in Class Freight Services</h1>
       <p>Freight services all across US</p>
-      <div className='hero-btns'>
+      <div className="hero-btns">
         <Button
-          className='btns'
-          buttonStyle='btn--primary'
-          buttonSize='btn--large'
-          onClick={console.log('hey')}
+          className="btns"
+          buttonStyle="btn--primary"
+          buttonSize="btn--large"
+          onClick={() =>
+            (window.location = "mailto:accounting@fmsfreightbrokersinc.com")
+          }
         >
-          CONTACT US
+          GET QUOTE
         </Button>
       </div>
     </div>
--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -2,25 +2,27 @@ import React from "react";
 import "../../App.css";
 
 export default function SignUp() {
-  return ( 
-  <ul>
-  <li> <h4> CEO : Miss Saeeda Khanam</h4> </li>
-              <li><a href="mailto:fmsfreightbrokersinc@gmail.com">Get Quote</a>
-            </li>
-              <li>
-                <span>Email</span> <i class="far fa-envelope"></i> :
-                fmsfreightbrokersinc@gmail.com{" "}
-                
-               
-              </li>
-              <li>
-                {" "}
-                <span>Phone</span> <i class="fas fa-phone"></i> : 916.280.4415{" "}
-              </li>
-              <li>
-                <span>Address</span> <i class="fas fa-map-marker-alt"></i> : PO BOX
-                292272, SACRAMENTO, CA 95829{" "}
-              </li>
-            </ul>);
-  
+  return (
+    <ul>
+      <li>
+        {" "}
+        <h4> Sales and Logistics: Farhad Mirza</h4>{" "}
+      </li>
+      <li>
+        <a href="mailto:fmsfreightbrokersinc@gmail.com">Get Quote</a>
+      </li>
+      <li>
+        <span>Email</span> <i class="far fa-envelope"></i> :
+        farhad@fmsfreightbrokersinc.com{" "}
+      </li>
+      <li>
+        {" "}
+        <span>Phone</span> <i class="fas fa-phone"></i> : +1-916-476-7689{" "}
+      </li>
+      <li>
+        <span>Address</span> <i class="fas fa-map-marker-alt"></i> : PO BOX
+        292272, SACRAMENTO, CA 95829{" "}
+      </li>
+    </ul>
+  );
 }
